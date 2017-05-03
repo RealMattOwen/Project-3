955af83c958b3de371d6234cabb98657b2a865fe
Matthew Owen <matty101@live.co.uk>
2017-05-03 22:54:51 +0100

    Updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var userCards = [                          // Array called 'userCards' with objects for each card and name value pairs of properties for each.
+var userCards = [                          // Array called 'userCards' with objects for each card and name : value pairs of properties for each.
   {
     Picture : 'images/user-icon.png',
     Name : 'Shawn Garner',
@@ -49,24 +49,22 @@ var userCards = [                          // Array called 'userCards' with obje
   }*/
 ];
 
-var i;                                     // Defining a variable called 'i', for use later or in the code.   
-
-var increment = 0;                         // Defining a variable called 'increment' and setting its value to '0'.
+var increment = 0;                         // Declaring a variable called 'increment' and setting its value to '0'.
 
 function updateCards(i) {                  // Function called updateCards, with a parameter of 'i', which passes the value current value of 'i' through to the function.
 
     for (i; i < userCards.length; i++) {   // For loop calling the value of 'i', then checking the condition of whether the value of 'i' is less than the length of the 'userCards' array, if it is then 'i' will increment by '1', if not it wont increment.
     
-        var userPicture = userCards[i].Picture;     // Defining a variable called 'userPicture' and storing the value of the 'Picture' keypair from the object that is in the 'userCards' array that is currently being accessed.
+        var userPicture = userCards[i].Picture;     // Declaring a variable called 'userPicture' and storing the value of the 'Picture' key from the object that is in the 'userCards' array that is currently being accessed.
                                                     // All objects in the array will be iterated through because the object that is being accessed is dependant on the value of 'i', which will keep incrementing until the number of objects is reached because of the for loop that it is within.
 
-        var userName = userCards[i].Name;           // Defining a variable called 'userName' and storing the value of the 'Name' keypair from the object that is in the 'userCards' array that is currently being accessed.
+        var userName = userCards[i].Name;           // Declaring a variable called 'userName' and storing the value of the 'Name' key from the object that is in the 'userCards' array that is currently being accessed.
                                                     // All objects in the array will be iterated through because the object that is being accessed is dependant on the value of 'i', which will keep incrementing until the number of objects is reached because of the for loop that it is within.
 
-        var userHandle = userCards[i].Handle;       // Defining a variable called 'userHandle' and storing the value of the 'Handle' keypair from the object that is in the 'userCards' array that is currently being accessed.
+        var userHandle = userCards[i].Handle;       // Declaring a variable called 'userHandle' and storing the value of the 'Handle' key from the object that is in the 'userCards' array that is currently being accessed.
                                                     // All objects in the array will be iterated through because the object that is being accessed is dependant on the value of 'i', which will keep incrementing until the number of objects is reached because of the for loop that it is within.
         
-        var userTasks = userCards[i].Tasks;         // Defining a variable called 'userTasks' and storing the value of the 'Tasks' keypair from the object that is in the 'userCards' array that is currently being accessed.
+        var userTasks = userCards[i].Tasks;         // Declaring a variable called 'userTasks' and storing the value of the 'Tasks' key from the object that is in the 'userCards' array that is currently being accessed.
                                                     // All objects in the array will be iterated through because the object that is being accessed is dependant on the value of 'i', which will keep incrementing until the number of objects is reached because of the for loop that it is within.
 
         var card = '<div id="card' + i + '" class="card searchRef">\
@@ -93,48 +91,50 @@ function updateCards(i) {                  // Function called updateCards, with 
                         </div>\
                     </div>';
 
-        // Defining a variable called 'card' and storing the HTML and JS for the creation of a new card instead of copy and pasting it 8 times, 1 time for each card, it makes it easier to define it once as a variable and reuse it.
+        // Declaring a variable called 'card' and storing the HTML and JS for the creation of a new card instead of copy and pasting it 8 times, 1 time for each card, it makes it easier to declare it once as a variable and reuse it.
         // I used '\' at the end of each line to utilise 'multi-line' javascript, which allows you to continue your code onto the next line but have it act as if it were on one line.
         // IMPORTANT: It is important to note that no spaces are left after the '\' at the end of the line as it will cause your code not to work.
-        // Within the variable 'card', you can see the previously created variables: 'i', 'userPicture', 'userName', 'userHandle' and 'userTasks' being used, the values of each are being concatenated to the releveant line of HTML.
+        // Within the variable 'card', you can see the previously created variables: 'i', 'userPicture', 'userName', 'userHandle' and 'userTasks' being used, the values of each are being concatenated to the relevant line of HTML.
         // The variable 'i' is being used to create a unique id for every card created by being concatenated with the current id of 'card', because the id is utilising the variable 'i' which will be incremented every time the loop runs, each card will be given its own unique id.
-        
+
+        var addCard = $('#add-card'); // Declaring a variable called 'addCard' and storing the HTML element with the id of 'add-card' this is so that it can be used later on in the code without having to be redeclared multiple times.
+
         increment++;        // On this line you can see the variable 'increment' being incremented by one.
 
         switch(i) {         // Switch statement, runs appropriate case code depending on which case value matches the current value of 'i'.
                             // Switch statements can accept numbers as well as strings, be sure to enclose the case values within apostrophes, not sure what everyone else calls them, but these things ''.
-            case 0:
-                $('#add-card').before(card);
+            case 0:         // Example: case '0' runs if the value of 'i' is 0 and so on.
+                addCard.before(card);
                 $('#card0').hide().fadeIn(1000);
                 break;
             case 1:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card1').hide().fadeIn(1000);
                 break;
             case 2:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card2').hide().fadeIn(1000);
                 break;
             case 3:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card3').hide().fadeIn(1000);
                 break;
             case 4:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card4').hide().fadeIn(1000);
                 break;
             case 5:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card5').hide().fadeIn(1000);
                 break;
             case 6:
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card6').hide().fadeIn(1000);
                 break;
             case 7: 
-                $('#add-card').before(card);
+                addCard.before(card);
                 $('#card7').hide().fadeIn(1000);
-                $('#add-card').remove();
+                addCard.remove();
                 break;
             default:
                 break;
@@ -144,10 +144,10 @@ function updateCards(i) {                  // Function called updateCards, with 
 
 }
 
-var pictureValidationPass = "false";
-var nameValidationPass = "false";
-var handleValidationPass = "false";
-var taskValidationPass = "false";
+var pictureValidationPass = 'false';
+var nameValidationPass = 'false';
+var handleValidationPass = 'false';
+var taskValidationPass = 'false';
 
 var pictureInput = document.getElementById('picture');
 var nameInput = document.getElementById('name');
@@ -160,35 +160,35 @@ var letters = /^[a-zA-Z\s]+$/;
 var lettersAndNumbers = /^[a-zA-Z0-9_]+$/;
 var numbers = /^[0-9]+$/;
 
-pictureInput.oninput = function() {
+pictureInput.onInput = function() {
     if ((this.value.match(imageURL)) && (this.value !== blankInputCheck)) {
-        pictureValidationPass = "true";
+        pictureValidationPass = 'true';
     } else {
-        pictureValidationPass = "false";
+        pictureValidationPass = 'false';
     }
 };
 
-nameInput.oninput = function() {
+nameInput.onInput = function() {
     if (this.value.match(letters) && (this.value !== blankInputCheck)) {
-        nameValidationPass = "true";
+        nameValidationPass = 'true';
     } else {
-        nameValidationPass = "false";
+        nameValidationPass = 'false';
     }
 };
 
-handleInput.oninput = function() {
+handleInput.onInput = function() {
     if (this.value.match(lettersAndNumbers) && (this.value !== blankInputCheck)) {
-        handleValidationPass = "true";
+        handleValidationPass = 'true';
     } else {
-        handleValidationPass = "false";
+        handleValidationPass = 'false';
     }
 };
 
-taskInput.oninput = function() {
+taskInput.onInput = function() {
     if (this.value.match(numbers) && (this.value !== blankInputCheck)) {
-        taskValidationPass = "true";
+        taskValidationPass = 'true';
     } else {
-        taskValidationPass = "false";
+        taskValidationPass = 'false';
     }
 };
 
@@ -197,7 +197,7 @@ $('#form').submit(function(e) {
 
 		e.preventDefault();
 
-        if((pictureValidationPass !== "false") && (nameValidationPass !== "false") && (handleValidationPass !== "false") && (taskValidationPass !== "false")) {
+        if((pictureValidationPass !== 'false') && (nameValidationPass !== 'false') && (handleValidationPass !== 'false') && (taskValidationPass !== 'false')) {
             userCards.push({
                 Picture : pictureInput.value,
                 Name : nameInput.value,
@@ -250,7 +250,7 @@ $(document).ready(function() {
     });
 
     $("#modal").on("click",function(event) {
-        if (event.target == this) {
+        if (event.target === this) {
             $("#modal").fadeOut();
         }
     });
@@ -265,7 +265,7 @@ $(document).ready(function() {
 
     var input = document.getElementById('search-box');
 
-    input.oninput = function () {
+    input.onInput = function () {
         memberSearch();
     };
 
